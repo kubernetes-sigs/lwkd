8ba7206e97c4efc5f90ad6cc7f93dc9d7ae83afe
Sreeram <svenkitesh@digitalocean.com>
2025-01-26 23:35:53 +0530

    Make setup script fill branch and md header with correct date

--- script.sh
+++ script.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+git checkout main
+git pull origin main
+git checkout -b $(date -v -sun +"%Y%m%d")
 
-cp template.md ./_posts/$(date -v -sat +"%Y-%m-%d-update.md")
+NEW_DATE=$(date -v -sun +"%Y-%m-%d")
+cp template.md ./_posts/$NEW_DATE-update.md
+
+sed -i '' "s/2025-01-20/$(date -v -sun +"%Y-%m-%d")/g" ./_posts/$NEW_DATE-update.md
+sed -i '' "s/2025-01-22/$(date +"%Y-%m-%d")/g" ./_posts/$NEW_DATE-update.md
+sed -i '' "s/Month DD/$(date -v -sun +"%B %d")/g" ./_posts/$NEW_DATE-update.md
