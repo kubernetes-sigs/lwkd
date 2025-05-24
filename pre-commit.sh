696cbfc7b9c6367a4d040cc6811b7b1d78aa4710
sreeram-venkitesh <sreeramvenkitesh@gmail.com>
2025-05-24 15:34:38 +0530

    Added pre-commit script to automate setting date in posts

---
+++ pre-commit.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# To configure this pre-commit hook, copy this file to .git/hooks/pre-commit
+# Run `cp ./pre-commit.sh ./git/hooks/pre-commit`
+# This script will update the time date key in the issue's frontmatter
+# if the latest file in _posts is touched.
+
+filename=$(ls -t _posts | head -n1)
+filepath="_posts/$filename"
+
+if git status --porcelain $filepath | grep -q "^[ AM]  .*$filepath$"; then
+    echo "Executing pre-commit hook to set current date in $filepath" >&2
+    date=$(TZ=UTC date +"%Y-%m-%d %H:%M:%S -0000")
+
+    if [ "$(uname)" = "Darwin" ]; then
+        sed -i.bak "s/^date:.*$/date: $date/" "$filepath"
+        rm "${filepath}.bak"
+    else
+        sed -i "s/^date:.*$/date: $date/" "$filepath"
+    fi
+
+    git add "$filepath"
+fi
+
+exit 0
