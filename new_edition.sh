a03289ba39709c5e1fd7354e66f1349ff7763972
Kashish <kashishverma519@gmail.com>
2026-06-21 14:12:09 -0400

    Added mode for next or last sunday

--- new_edition.sh
+++ new_edition.sh
@@ -1,26 +1,49 @@
 #!/bin/sh
+set -e
+
+# --- Determine mode (next or last Sunday) ---
+MODE="${1:-next}"
+
+case "$MODE" in
+  next)
+    SUNDAY_FLAG_BSD="+sun"
+    SUNDAY_PHRASE_GNU="next sunday"
+    ;;
+  last)
+    SUNDAY_FLAG_BSD="-1sun"
+    SUNDAY_PHRASE_GNU="last sunday"
+    ;;
+  *)
+    echo "Usage: $0 [next|last]"
+    exit 1
+    ;;
+esac
+
 git checkout main
 git pull origin main
 
 if [ "$(uname)" = "Darwin" ]; then
-  git checkout -b $(date -v +sun +"%Y%m%d")
+  BRANCH_DATE=$(date -v "$SUNDAY_FLAG_BSD" +"%Y%m%d")
 else
-  git checkout -b $(date -d 'next sunday' +"%Y%m%d")
+  BRANCH_DATE=$(date -d "$SUNDAY_PHRASE_GNU" +"%Y%m%d")
 fi
+git checkout -b "$BRANCH_DATE"
 
 DATE_IN_TEMPLATE="POST_DATE"
 SLUG_IN_TEMPLATE="POST_SLUG_DATE"
 
 if [ "$(uname)" = "Darwin" ]; then
-    NEW_DATE=$(date -v +sun +"%Y-%m-%d")
+    NEW_DATE=$(date -v "$SUNDAY_FLAG_BSD" +"%Y-%m-%d")
+    MONTH_DAY_YEAR=$(date -v "$SUNDAY_FLAG_BSD" +"%B %d, %Y")
     cp template.md ./_posts/$NEW_DATE-update.md
     sed -i '' "s/$SLUG_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
     sed -i '' "s/$DATE_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
-    sed -i '' "s/Month DD/$(date -v +sun +"%B %d")/g" ./_posts/$NEW_DATE-update.md
+    sed -i '' "s/Month DD, YYYY/$MONTH_DAY_YEAR/g" ./_posts/$NEW_DATE-update.md
 else
-    NEW_DATE=$(date -d 'next sunday' +"%Y-%m-%d")
+    NEW_DATE=$(date -d "$SUNDAY_PHRASE_GNU" +"%Y-%m-%d")
+    MONTH_DAY_YEAR=$(date -d "$SUNDAY_PHRASE_GNU" +"%B %d, %Y")
     cp template.md ./_posts/$NEW_DATE-update.md
     sed -i "s/$SLUG_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
     sed -i "s/$DATE_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
-    sed -i "s/Month DD/$(date -d 'next sunday' +"%B %d")/g" ./_posts/$NEW_DATE-update.md
+    sed -i "s/Month DD, YYYY/$MONTH_DAY_YEAR/g" ./_posts/$NEW_DATE-update.md
 fi
