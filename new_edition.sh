802b3eacf60c2ca3de12082bc0bd568e364145de
Kashish <kashishverma519@gmail.com>
2026-06-21 14:26:05 -0400

    debug

--- new_edition.sh
+++ new_edition.sh
@@ -35,10 +35,29 @@ SLUG_IN_TEMPLATE="POST_SLUG_DATE"
 if [ "$(uname)" = "Darwin" ]; then
     NEW_DATE=$(date $SUNDAY_FLAGS_BSD +"%Y-%m-%d")
     MONTH_DAY_YEAR=$(date $SUNDAY_FLAGS_BSD +"%B %d, %Y")
+
+    echo ">>> DEBUG: NEW_DATE=[$NEW_DATE]"
+    echo ">>> DEBUG: MONTH_DAY_YEAR=[$MONTH_DAY_YEAR]"
+
     cp template.md ./_posts/$NEW_DATE-update.md
+
+    echo ">>> DEBUG: after cp, file contents:"
+    cat ./_posts/$NEW_DATE-update.md
+
     sed -i '' "s/$SLUG_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
+
+    echo ">>> DEBUG: after slug sed:"
+    grep -n "slug" ./_posts/$NEW_DATE-update.md
+
     sed -i '' "s/$DATE_IN_TEMPLATE/$NEW_DATE/g" ./_posts/$NEW_DATE-update.md
+
+    echo ">>> DEBUG: after date sed:"
+    grep -n "date:\|title" ./_posts/$NEW_DATE-update.md
+
     sed -i '' "s/Month DD, YYYY/$MONTH_DAY_YEAR/g" ./_posts/$NEW_DATE-update.md
+
+    echo ">>> DEBUG: after Month DD, YYYY sed:"
+    grep -n "title" ./_posts/$NEW_DATE-update.md
 else
     NEW_DATE=$(date -d "$SUNDAY_PHRASE_GNU" +"%Y-%m-%d")
     MONTH_DAY_YEAR=$(date -d "$SUNDAY_PHRASE_GNU" +"%B %d, %Y")
